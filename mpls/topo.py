8929170c6af5aa4f3894e0181521d98b7f8fe754
Tomek Osiński <osinstom@gmail.com>
2019-03-03 14:11:40 +0000

    Simple MPLS demo in P4

--- mpls/topo.py
+++ mpls/topo.py
@@ -40,17 +40,12 @@ class DemoTopo(Topo):
         h2 = self.addHost('h2',
                           ip="10.0.20.10/24",
                           mac='00:00:00:00:00:02')
-        h3 = self.addHost('h3',
-                          ip="10.0.30.10/24",
-                          mac='00:00:00:00:00:03')
 
         self.addLink(s1, h1)
-        self.addLink(s2, h2)
-        self.addLink(s3, h3)
+        self.addLink(s3, h2)
 
         self.addLink(s1, s2)
         self.addLink(s2, s3)
-        self.addLink(s1, s3)
 
 
 def main():
@@ -68,33 +63,24 @@ def main():
     s1.setMAC('00:00:00:00:01:01', intf = 's1-eth1')
     s1.setIP('192.168.1.1/24', intf = 's1-eth2')
     s1.setMAC('00:00:00:00:01:02', intf='s1-eth2')
-    s1.setIP('192.168.3.254/254', intf = 's1-eth3')
-    s1.setMAC('00:00:00:00:01:03', intf='s1-eth3')
 
     s2 = net.get('s2')
-    s2.setIP('10.0.20.1/24', intf = 's2-eth1')
+    s2.setIP('192.168.1.254/24', intf = 's2-eth1')
     s2.setMAC('00:00:00:00:02:01', intf='s2-eth1')
-    s2.setIP('192.168.3.1/24', intf = 's2-eth2')
+    s2.setIP('192.168.2.254/24', intf = 's2-eth2')
     s2.setMAC('00:00:00:00:02:02', intf='s2-eth2')
-    s2.setIP('192.168.2.254/24', intf = 's2-eth3')
-    s2.setMAC('00:00:00:00:02:03', intf='s2-eth3')
 
     s3 = net.get('s3')
-    s3.setIP('10.0.30.1/24', intf='s3-eth1')
+    s3.setIP('10.0.20.1/24', intf='s3-eth1')
     s3.setMAC('00:00:00:00:03:01', intf='s3-eth1')
     s3.setIP('192.168.2.1', intf='s3-eth2')
     s3.setMAC('00:00:00:00:03:02', intf='s3-eth2')
-    s3.setIP('192.168.1.254/24', intf='s3-eth3')
-    s3.setMAC('00:00:00:00:03:03', intf='s3-eth3')
 
     h1 = net.get('h1')
     h1.setDefaultRoute("dev eth0 via 10.0.10.1")
 
     h2 = net.get('h2')
     h2.setDefaultRoute("dev eth0 via 10.0.20.1")
-
-    h3 = net.get('h3')
-    h3.setDefaultRoute("dev eth0 via 10.0.30.1")
 
     print "Ready !"
 
